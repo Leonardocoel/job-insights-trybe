e0a9d4f231f789aebdc04da8632c8c828cedeaf8
Leonardo C <leonardocoel98@gmail.com>
2022-10-21 19:48:51 -0300

    test: implement tests for read brazilian file function

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,8 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    result = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+
+    for job in result:
+        assert list(job.keys()) == ["title", "salary", "type"]
