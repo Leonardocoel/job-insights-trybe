7f6dbf82b761a09e5e6bf3bd9b71a0b11eeb571d
Leonardo C <leonardocoel98@gmail.com>
2022-10-20 23:37:57 -0300

    feat: implement read function in jobs.py

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,10 @@
+import csv
 from functools import lru_cache
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+    with open(path, encoding="utf8") as file:
+        file_reader = csv.DictReader(file, delimiter=",", quotechar='"')
+        file_list = list(file_reader)
+        return file_list
