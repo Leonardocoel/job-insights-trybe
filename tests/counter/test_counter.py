38852034fd7247925b6dd46d1e827d627284199c
Leonardo C <leonardocoel98@gmail.com>
2022-10-21 19:11:22 -0300

    test: implement tests for counter_ocurrencies function

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,10 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    result_lower = count_ocurrences("src/jobs.csv", "python")
+    result_upper = count_ocurrences("src/jobs.csv", "PYTHON")
+
+    assert result_lower == 1639
+    assert result_upper == 1639
+    assert result_upper == result_lower
