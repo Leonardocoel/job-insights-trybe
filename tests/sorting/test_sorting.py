c6ff3e0bbd4f1263d8b1e24b9e5d6e1a7cb0ca3f
Leonardo C <leonardocoel98@gmail.com>
2022-10-24 10:17:38 -0300

    test: implement tests for sort by criteria function

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,82 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+
+
+jobs = [
+    {
+        "min_salary": 1000,
+        "max_salary": 3000,
+        "date_posted": "2020-05-07",
+    },
+    {
+        "min_salary": 3000,
+        "max_salary": 10000,
+        "date_posted": "2020-05-02",
+    },
+    {
+        "min_salary": 2000,
+        "max_salary": 5000,
+        "date_posted": "2020-12-02",
+    },
+]
+
+jobs_mock = [
+    [
+        {
+            "min_salary": 1000,
+            "max_salary": 3000,
+            "date_posted": "2020-05-07",
+        },
+        {
+            "min_salary": 2000,
+            "max_salary": 5000,
+            "date_posted": "2020-12-02",
+        },
+        {
+            "min_salary": 3000,
+            "max_salary": 10000,
+            "date_posted": "2020-05-02",
+        },
+    ],
+    [
+        {
+            "min_salary": 3000,
+            "max_salary": 10000,
+            "date_posted": "2020-05-02",
+        },
+        {
+            "min_salary": 2000,
+            "max_salary": 5000,
+            "date_posted": "2020-12-02",
+        },
+        {
+            "min_salary": 1000,
+            "max_salary": 3000,
+            "date_posted": "2020-05-07",
+        },
+    ],
+    [
+        {
+            "min_salary": 2000,
+            "max_salary": 5000,
+            "date_posted": "2020-12-02",
+        },
+        {
+            "min_salary": 1000,
+            "max_salary": 3000,
+            "date_posted": "2020-05-07",
+        },
+        {
+            "min_salary": 3000,
+            "max_salary": 10000,
+            "date_posted": "2020-05-02",
+        },
+    ],
+]
 
 
 def test_sort_by_criteria():
-    pass
+    criterias = ["min_salary", "max_salary", "date_posted"]
+
+    for i in range(len(criterias)):
+        sort_by(jobs, criterias[i])
+        assert jobs == jobs_mock[i]
